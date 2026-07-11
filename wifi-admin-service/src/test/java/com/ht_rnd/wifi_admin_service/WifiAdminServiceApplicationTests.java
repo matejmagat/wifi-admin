57aec53ec92a67809c912806dce4e2bd6e5b7602
Matej Magat <matej.magat2@gmail.com>
2026-07-11 21:38:33 +0200

    initialized the spring boot project

---
+++ wifi-admin-service/src/test/java/com/ht_rnd/wifi_admin_service/WifiAdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ht_rnd.wifi_admin_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WifiAdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
