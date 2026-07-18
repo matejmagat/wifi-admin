164a19afff8655be1746dff61c7456468df3f64e
Matej Magat <matej.magat2@gmail.com>
2026-07-18 17:30:03 +0200

    added persistence layer

---
+++ wifi-admin-service/src/main/java/com/ht_rnd/wifi_admin_service/config/SchedulerConfig.java
@@ -0,0 +1,4 @@
+package com.ht_rnd.wifi_admin_service.config;
+
+public class SchedulerConfig {
+}
---
+++ wifi-admin-service/src/main/java/com/ht_rnd/wifi_admin_service/scheduler/WifiSyncScheduler.java
@@ -0,0 +1,4 @@
+package com.ht_rnd.wifi_admin_service.scheduler;
+
+public class WifiSyncScheduler {
+}
